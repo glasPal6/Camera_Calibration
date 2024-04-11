cc51b65f694d14a0686a5ef8cac737219d01bb73
glaspal6 <dylan.kamstra@protonmail.com>
2024-04-11 09:30:42 +0200

    Added radial distortion to the optimization

--- zhang.py
+++ zhang.py
@@ -293,8 +293,7 @@ def parameter_refinement_loss_func(world_points, *params):
 
     img_points = []
     for E in E_ext:
-        u_proj = reproject_pin_hole(world_points.T, K_int, E)
-
+        u_proj = reproject_radial_distortion(world_points.T, K_int, E, k_rad)
         img_points.append(u_proj[:2, :].T)
     img_points = np.array(img_points)
 
@@ -317,7 +316,16 @@ def reproject_pin_hole(world_points, K_int, E_ext):
     return u_proj[:2, :]
 
 def reproject_radial_distortion(world_points, K_int, E_ext, k_rad):
-    raise NotImplementedError
+    camera_points = (E_ext @ world_points)
+    camera_points = camera_points[:2, :] / camera_points[2, :]
+
+    r = np.linalg.norm(camera_points, axis=0).reshape((1, -1))
+    distortion_warping = 1 + k_rad[0] * np.power(r, 2) + k_rad[1] * np.power(r, 4)
+    distorted_points = distortion_warping * camera_points
+
+    projected_points = K_int[:2, :] @ np.vstack([distorted_points, np.ones((1, distorted_points.shape[1]))])
+
+    return projected_points
 
 #-------------------------------------------------------------------------------
 
